d3ad751fdd33900a13477aa2dae72fd83766866f
niloy8 <99067134+niloy8@users.noreply.github.com>
2025-02-01 09:36:01 +0000

    Form added to send data in server

--- NewServer/ClientSide/src/App.jsx
+++ NewServer/ClientSide/src/App.jsx
@@ -1,12 +1,36 @@
 
+import { application } from 'express'
 import './App.css'
 
 function App() {
+  // const handleSubmit = e => {
+  //   const form = e.target
+  //   const name = form.name.value
+  //   const email = form.email.value
+  //   const user = { name, email }
+  //   fetch("https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch", {
+  //     method: 'POST',
+  //     headers: {
+  //       'content-type': "application/json"
+  //     },
+  //     body: JSON.stringify(user)
+
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  // }
 
 
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="name" />
+        <input type="email" name="email" id="email" />
+        <input type="submit" value="SUBMIT NOW" />
+      </form>
     </>
   )
 }
